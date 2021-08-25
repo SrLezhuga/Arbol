135ac41eb8d766f029a3d7dd8452e87ca933ca93
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-08-24 19:09:09 -0500

    .
    .

---
+++ controller/preguntas/precarga.php
@@ -0,0 +1,39 @@
+<?php
+include "../conexion.php";
+
+if ($_POST['Rs'] == 'ok') {
+  $query = $con->prepare("SELECT pregunta, respuesta FROM tab_preguntas WHERE activo = 'Y'");
+  $query->setFetchMode(PDO::FETCH_ASSOC);
+  $query->execute();
+  $count = 1;
+
+  while ($row = $query->fetch()) {
+    $show = ($count == 1) ? $show = "show" : $show = "";
+    $bool = ($count == 1) ? $bool = "true" : $bool = "false";
+    $collapsed = ($count == 1) ? $collapsed = "" : $collapsed = "collapsed";
+
+    echo '
+    
+    <!-- Pregunta ' . $count . ' -->
+    <div class="accordion-item rounded shadow-lg">
+      <h2 class="accordion-header" id="heading' . $count . '">
+        <button class="accordion-button ' . $collapsed . '" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' . $count . '" aria-expanded="' . $bool . '" aria-controls="collapse' . $count . '">
+        ' . $count . '. ' . $row["pregunta"] . '
+        </button>
+      </h2>
+      <div id="collapse' . $count . '" class="accordion-collapse collapse ' . $show . '" aria-labelledby="heading' . $count . '" data-bs-parent="#accordionPreguntas">
+        <div class="accordion-body">
+          <p class="small">
+          ' . nl2br($row["respuesta"]) . '
+          </p>
+        </div>
+      </div>
+    </div>
+    <br>
+
+    ';
+    $count++;
+  }
+} else {
+  return false;
+}
--- preguntas.php
+++ preguntas.php
@@ -24,73 +24,9 @@
     <section>
       <div class="container mt-5 mb-3">
         <div class="justify-content-center align-items-center">
-
-          <div class="accordion" id="accordionExample">
-
-            <!-- 1 -->
-            <div class="accordion-item rounded shadow-lg">
-              <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  1. ¿POR QUÉ ELEGIR COMPRAR CON USTEDES?
-                </button>
-              </h2>
-              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <p class="small">Porque tenemos más de 30 años de experiencia en el mercado, somo una empresa consolidada en la cual puedes confiar. Siempre trabajamos para darte el mejor servicio y tenerte las mejores refacciones para tus clientes. Constantemente nos estamos renovando para ser tu mejor opción en mayoreo de autopartes.</p>
-                </div>
-              </div>
-            </div>
-            <br>
-
-            <!-- 2 -->
-            <div class="accordion-item rounded shadow-lg">
-              <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  2. ¿ES SEGURO COMPRAR CON USTEDES?
-                </button>
-              </h2>
-              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <p class="small">Claro, todos los pedidos que recibimos los tomamos con mucha seriedad, y sabemos que somos parte fundamental para que logres tener éxito en tu refaccionaria. Buscamos siempre cumplir con los mayores estándares de servicio y entrega.</p>
-                </div>
-              </div>
-            </div>
-            <br>
-
-            <!-- 3 -->
-            <div class="accordion-item rounded shadow-lg">
-              <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  3. ¿DÓNDE ESTÁN UBICADOS?
-                </button>
-              </h2>
-              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <p class="small">Tenemos dos sucursales, nuestra Matriz en Guadalajara y el Centro de Distribución en Monterrey.</p>
-                </div>
-              </div>
-            </div>
-            <br>
-
-            <!-- 4 -->
-            <div class="accordion-item rounded shadow-lg">
-              <h2 class="accordion-header" id="headingFour">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                  4. SOY DE OTRO ESTADO DONDE NO TIENEN SUCURSAL ¿PUEDO COMPRAR?
-                </button>
-              </h2>
-              <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <p class="small">¡Claro! Sin problema. Hacemos envíos a toda la República Mexicana.</p>
-                </div>
-              </div>
-            </div>
-            <br>
-
-            <!-- 1 -->
-          </div>
-
-
+          <!-- Preguntas -->
+          <div class="accordion" id="accordionPreguntas"></div>
+          <!-- /Preguntas -->
         </div>
       </div>
     </section>
@@ -101,6 +37,28 @@
 
 </body>
 
-
-
 </html>
+
+<script>
+  $(document).ready(function() {
+    var busqueda = 'ok';
+    $.ajax({
+      url: "controller/preguntas/precarga.php",
+      type: "post",
+      data: {
+        Rs: busqueda
+      },
+      beforeSend: function() {
+        //imagen de carga
+        $("#accordionPreguntas").html("<div class='col-12'><img src='assets/media/img/loader/loader.gif'></div>");
+      },
+      error: function() {
+        alert("error petición ajax");
+      },
+      success: function(data) {
+        $("#accordionPreguntas").empty();
+        $("#accordionPreguntas").append(data);
+      }
+    });
+  });
+</script>
